09114ee1af8115504b49c9204be3216180dcd4be
Harun Petekkaya <harunpe2107@gmail.com>
2021-02-01 09:57:17 +0300

    Interfacev2

---
+++ Interfaces/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+
+        //interface new lenemez
+        static void Main(string[] args)
+        {
+            IPersonManager customerManager = new CustomerManager();
+            //customerManager.Add();
+
+            IPersonManager employeeManager = new EmployeeManager();
+           // employeeManager.Add();
+
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(customerManager);
+            projectManager.Add(employeeManager);
+            projectManager.Add(new InternManager());
+        }
+    }
+
+    interface IPersonManager
+    {
+        //unimplemented operation
+        void Add();
+        void Update();
+    }
+
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            //musteri ekleme kodlari
+            Console.WriteLine("Musteri Eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Musteri Guncellendi");
+        }
+    }
+
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Personel Eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Personel Guncellendi");
+        }
+    }
+
+    class InternManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Stajyer Eklendi");
+
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Stajyer Guncellendi");
+
+        }
+    }
+
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager)
+        {
+            personManager.Add();
+        }
+    }
+}
---
+++ Interfaces_v2/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interfaces_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<IKrediManager> krediler = new List<IKrediManager> {
+
+            //    new TasitKrediManager(),
+            //    new IhtiyacKrediManager(),
+            //    new KonutKrediManager()
+            
+            //};
+
+            List<ILoggerService> loglar = new List<ILoggerService> { 
+            
+                new DatabaseLoggerService(),
+                new FileLoggerService(),
+                new SmsLoggerService()
+            };
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYapVeLogla(new TasitKrediManager(), loglar);
+
+
+        }
+    }
+
+    public interface IKrediManager
+    {
+        void Hesapla();
+
+    }
+
+    public interface ILoggerService
+    {
+        void Log();
+    }
+
+
+    class DatabaseLoggerService : ILoggerService
+    {
+        public void Log()
+        {
+            Console.WriteLine("Veritabanina Loglandi");
+        }
+    }
+
+    class FileLoggerService : ILoggerService
+    {
+        public void Log()
+        {
+            Console.WriteLine("Dosyaya Loglandi");
+        }
+    }
+
+    class SmsLoggerService : ILoggerService
+    {
+        public void Log()
+        {
+            Console.WriteLine("Sms ile gonderilip Loglandi");
+        }
+    }
+
+    class KonutKrediManager : IKrediManager
+    {
+        public void Hesapla()
+        {
+            Console.WriteLine("Konut Kredisi Hesaplandi");
+        }
+    }
+
+    class IhtiyacKrediManager : IKrediManager
+    {
+        public void Hesapla()
+        {
+            Console.WriteLine("Ihtiyac Kredisi Hesaplandi");
+        }
+    }
+
+    class TasitKrediManager : IKrediManager
+    {
+        public void Hesapla()
+        {
+            Console.WriteLine("Tasit Kredisi Hesaplandi");
+        }
+    }
+
+    class BasvuruManager
+    {
+        public void BasvuruYapVeLogla(IKrediManager kredi,List<ILoggerService> loglar)
+        {
+            kredi.Hesapla();
+
+            foreach (var log in loglar)
+            {
+                log.Log();
+            }
+        }
+    }
+}
